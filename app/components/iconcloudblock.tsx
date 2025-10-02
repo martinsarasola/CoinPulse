642b67dcccdf6f6354c62ea339620d1639abdf7e
José <martin.sarasola01@hotmail.com>
2025-10-01 21:16:56 -0300

    Modified icons quantity on iconcloudblock

--- app/components/iconcloudblock.tsx
+++ app/components/iconcloudblock.tsx
@@ -5,7 +5,8 @@ import type { Criptomoneda } from "@/app/types/crypto";
 export const IconCloudBlock = () => {
   const { data, isLoading } = useCryptoData();
 
-  const imagenes = data?.map((coin: Criptomoneda) => coin.imagen) ?? [];
+  const imagenes =
+    data?.slice(0, 20).map((coin: Criptomoneda) => coin.imagen) ?? [];
 
   if (isLoading) return null;
 

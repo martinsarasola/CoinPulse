710120f25364a78b6206f29d86ec53eb7bbed1be
José <martin.sarasola01@hotmail.com>
2025-10-03 19:05:09 -0300

    Deleted iconcloudblock

--- app/components/mainpage.tsx
+++ app/components/mainpage.tsx
@@ -37,8 +37,6 @@ export default function MainPage() {
             <Link href="/dashboard">
               <FlowButton text="Comenzar"></FlowButton>
             </Link>
-
-            <IconCloudBlock></IconCloudBlock>
           </div>
         </GlowCard>
       </motion.div>

7bb1fbfdc8e62fcc65b763a61b5c93bf57d4b728
José <martin.sarasola01@hotmail.com>
2025-10-03 19:12:48 -0300

    deleted motion.div

--- app/components/mainpage.tsx
+++ app/components/mainpage.tsx
@@ -15,31 +15,22 @@ export default function MainPage() {
 
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden border">
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="flex flex-col gap-4 items-center justify-center px-4 max-w-8/12"
-      >
-        <GlowCard customSize={true} glowColor="blue" className="w-auto">
-          <div className="flex flex-col items-center justify-center gap-2 md:px-4 pt-4">
-            <div className="text-3xl md:text-5xl font-bold text-white text-center">
-              LA BRUJULA DE TUS ACTIVOS DIGITALES
-            </div>
-            <div className="font-extralight text-base md:text-2xl text-white py-4 text-center">
-              Mantente informado, toma mejores decisiones.
-            </div>
-
-            <Link href="/dashboard">
-              <FlowButton text="Comenzar"></FlowButton>
-            </Link>
+      <GlowCard customSize={true} glowColor="blue" className="w-auto">
+        <div className="flex flex-col items-center justify-center gap-2 md:px-4 pt-4">
+          <div className="text-3xl md:text-5xl font-bold text-white text-center">
+            LA BRUJULA DE TUS ACTIVOS DIGITALES
           </div>
-        </GlowCard>
-      </motion.div>
+          <div className="font-extralight text-base md:text-2xl text-white py-4 text-center">
+            Mantente informado, toma mejores decisiones.
+          </div>
+
+          <Link href="/dashboard">
+            <FlowButton text="Comenzar"></FlowButton>
+          </Link>
+
+          <IconCloudBlock></IconCloudBlock>
+        </div>
+      </GlowCard>
     </div>
   );
 }
